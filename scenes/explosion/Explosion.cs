9ad9c73dd5095db924b758c64343baf6b56e0484
King <allenhartley@proton.me>
2025-06-25 18:09:06 +0200

    added animated player sprite

---
+++ scenes/explosion/Explosion.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+
+public partial class Explosion : AnimatedSprite2D
+{
+	public override void _Ready()
+	{
+		AnimationFinished += OnAnimationFinished;
+	}
+
+	private void OnAnimationFinished()
+    {
+        GD.Print("Animation finished playing!");
+        QueueFree(); 
+    }
+}
